50102e70c4144a23b1296310d02a80c51086afec
austeneriksson <78045228+austeneriksson@users.noreply.github.com>
2021-02-24 15:02:23 -0700

    Attempt to fix diffusion

--- Diffusion.m
+++ Diffusion.m
@@ -1,30 +1,31 @@
-function [Cs,Sb,bflux]=diffusion(Sbold,LL,S(i),mumax,Xb,Yxs,De,Km,Daq,dz,Nz)
+function [Cs,Sb,bflux]=diffusion(Sbold,LL,S,mumax,Xb,Yxs,De,Km,Daq,Lf,dz)
 %% This function models the diffusion of a substrate within the biofilm
 %This Function will take tank conditions (So,Xb,LL) and various growth factors (Yxs,De,Km,Daq) model the diffusion of
 % substrates into the biofilm over the grid . The results of this uptake will be used to
 % model the manner in which tank conditions reach equilibrium
-
-Sb=zeros(1,Nz);
+ Nz=50;
+ 
+ Sb=zeros(1,Nz);
 
 %Iterations
 for iter=1:1000
-muiter=mu(Sbold,mumax,Km); %To compute Sb
+%muiter=mu(Sb,mumax,Km); %To compute Sb
         c=2:1:Nz-1; %array to run concentration calculations through
-        Sb(c)=(Sbold(c+1)+Sbold(c-1)-(muiter(c)*Xb*(dz^2))/(Yxs*De))/2; %Concentration of substrate at biofilm depth
+        Sb(c)=(Sbold(c+1)+Sbold(c-1)-(mu(Sbold(c),mumax,Km)*Xb*(dz^2))/(Yxs*De))/2; %Concentration of substrate at biofilm depth
     
         %Boundary Conditions After Iteration
         Sb(1)=Sb(2);
-        Sb(end)=Sbold(end);
-        
+        Sb(end)=S;
+
         %Flux Calculations
-        bflux=(Sb(end)-Sb(end-1))/dz; %Biofilm Flux at boundary
+        bflux=(Sb(end)-Sb(end-1)); %Biofilm Flux at boundary
               
         %Flux Matching       
-        Sb(end)=So-LL*De*(bflux/Daq); %If LL=0 Boundary Concentration is Tank Concentration
+        %Sb(end)=S-LL*De*(bflux/(dz*Daq)); %If LL=0 Boundary Concentration is Tank Concentration
         
-        %Non Zero Condition
+         %Non Zero Condition
         Sb(Sb < 0) = 0;
-
+        
          Sbold=Sb;
 end
 Cs=Sb(end); %output Surface Concentration
--- MAINDRIVER.m
+++ MAINDRIVER.m
@@ -1,13 +1,13 @@
-function MAINDRIVER
+ %MAINDRIVER
 clear; clc
 
-% Inputs
+%% Inputs
 
 %Array Initial Conditions
 xo=10; %[g/m^-3] initial biomass concentration in bulk liquid
 So=25; %[g/m^-3] initial substrate concentration in bulk liquid
 
-%Tank Parameters + Geometry
+% Tank Parameters + Geometry
 L=0.5; %[m]
 W=0.5; %[m]
 H=0.4; %[m]
@@ -16,52 +16,53 @@ SA=(V/H)+2*((V/L)+(V/W)); %tank surface area [m^2]
 Qdot=1; %flow rate in/out [m^3]
 Sin=25; %Inflow of substrates to tank, [g/m^3]
 
-%Biofilm Parameters
+% Biofilm Parameters
 mumax=20; %max specific growth rate
 Km=3; %Monod half-saturation coefficient(growth transitions from sat. to linear)
 Yxs=0.5; %ratio of substrate consumed to biomass produced
 Daq=4.00e-5; %diffusion coefficient assumed at boundary [m/s^2]
+
 Lfo=5.00E-6; %biofilm thickness [m]
 Lf_old=Lfo;
+Lf=Lfo;
+
 LL=1.00e-4; %thickness of biofilm boundary layer [m]
 Co=So; %substrate concentration
 Xb=20000; %biomass density in biofilm [g/m^3]
-De=1.00E-05; %effective diffusion coefficient of substrate in biofilm [m^2/d^1]
+De=1.00e-5; %effective diffusion coefficient of substrate in biofilm [m^2/d^1]
 Kdet=1900; %coefficient of detachment for biofilm [m-1*s-1]
 
-Nz=100; %Linear GridPoints in Biofilm
-zo=linspace(0,Lfo,Nz); %[m] Grid of Biofilm Depth
-dzo=zo(2)-zo(1); %[m]
+Nz=50; %Linear GridPoints in Biofilm
+z=linspace(0,Lfo,Nz); %[m] Grid of Biofilm Depth
+dz=z(2)-z(1); %[m]
 
 %Initial Boundary Conditions (in Biofilm)
 Sb=zeros(1,Nz);
 Sb(end)=So; %initially assume boundary concentration = So
 
-%Time Constraints
+% Time Constraints
 tFin=2; %[s]
 dt=1e-3; %Interval
 N=tFin/dt; %Number of steps
 outFreq=10; %Number of steps between plot updates.
 
-%Preallocation
+% Preallocation
 t = zeros(1,N); %Time
 x = zeros(1,N); %Biomass Concentration in bulk liquid
 S = zeros(1,N); %Substrate in bulk liquid
 
-bflux=zeros(1,N); %Boundary Layer Flux of Biofilm
+bflux=zeros(1,N); %Boundary Layer Flux of Biofilm Preallocate
 
-
-%Initial Conditions
+% Initial Conditions
 t(1)=0;
 x(1)=xo;
 S(1)=So;
-Lf=Lfo;
 
 % Initialize plots 
 outIter=outFreq-1;
 plots=0;
 
-count=1;
+%% Time Loop
 for i = 1:N-1
     % Insert loop over spacial coordinate zeta for substrate diffusion,
     % particulates, biomass growth within biofilm, etc
@@ -70,10 +71,10 @@ for i = 1:N-1
     
     %Calculate Input Parameters
     Sbold=Sb; %Solve Diffusion with previous solution for efficiency
+    [Lf,Vdet]=lf(Sbold,Lf_old,Kdet,mumax,Km,dt,dz);
     z=linspace(0,Lf,Nz); %[m] Grid of Biofilm Depth
     dz=z(2)-z(1); %[m]
- 
-    [Cs,Sb,bflux(i)]=diffusion(Sbold,LL,S(i),mumax,Xb,Yxs,De,Km,Daq,dz,Nz);
+    [Cs,Sb,bflux]=diffusion(Sbold,LL,S(i),mumax,Xb,Yxs,De,Km,Daq,Lf,dz);
     
     %Call on Biofilm Thickness and Vdet/Vg from 'BiofilmThickness_Fn'
     Lf_old=Lf;
@@ -81,17 +82,12 @@ for i = 1:N-1
     
     %Call on tank substrate/biomass concentration from 'tankenvironment'
     [t(i+1),x(i+1),S(i+1)]=tankenvironment(t(i),x(i),S(i),V,SA,Qdot,Sin,Vdet,mumax,Km,Yxs,Daq,LL,Cs,Co,Xb,dt);
-    
+   
     %Call on desired plots from 'outputs'
     outIter=outIter+1;
     if (outIter==outFreq)
         [plots] = outputs(t(1:i+1),x(1:i+1),S(1:i+1),z,bflux,Sb,plots);
         outIter=0;
     end
-count=count+1;
+    
 end
-end
-
-
-
-
