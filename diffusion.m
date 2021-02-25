ca89e10139cec878e2e045a126e17573891066b7
austeneriksson <78045228+austeneriksson@users.noreply.github.com>
2021-02-25 16:24:22 -0700

    New Equation for Flux Matching Derived that does not produce erroneous concentrations at film boundary.

--- diffusion.m
+++ diffusion.m
@@ -3,41 +3,38 @@ function [Cs,Sb,bflux]=diffusion(Sbold,LL,S,mumax,Xb,Yxs,De,Km,Daq,Lf,dz)
 %This Function will take tank conditions (So,Xb,LL) and various growth factors (Yxs,De,Km,Daq) model the diffusion of
 % substrates into the biofilm over the grid . The results of this uptake will be used to
 % model the manner in which tank conditions reach equilibrium
- Nz=50;
+Nz=50;
+Sb=zeros(1,Nz);
+
  
- Sb=zeros(1,Nz);
-%Sbold=zeros(1,Nz);
-lamda=.5;
+lamda=.5; %Factor For Over Relaxation Method
 tol=1e-2;
 
 %Iterations
 for iter=1:10000
-%muiter=mu(Sb,mumax,Km); %To compute Sb
         c=2:1:Nz-1; %array to run concentration calculations through
         Sb(c)=(Sbold(c+1)+Sbold(c-1)-(mu(Sbold(c),mumax,Km)*Xb*(dz^2))/(Yxs*De))/2; %Concentration of substrate at biofilm depth
-        Sb(c)=lamda*Sb(c)+(1-lamda)*Sbold(c);
+        Sb(c)=lamda*Sb(c)+(1-lamda)*Sbold(c); %Over Relaxation Method
+        
         %Boundary Conditions After Iteration
-        Sb(1)=Sb(2);
+        Sb(1)=Sb(2); %Zero Flux at bottom Boundary
 %        Sb(end)=S;
-        Sb(end)=Sbold(end);
+%        Sb(end)=Sbold(end);
+        Sb(end)=((Daq/LL)*S+(De/dz)*Sb(end-1))/((De/dz)+(Daq/LL)); %Flux Matching At Top Border
 
-         %Flux Calculations
-         bflux=(Sb(end)-Sb(end-1))/dz; %Biofilm Flux at boundary
-      
+%          %Flux Matching     
+%          Sb(end)=S-LL*De*(bflux/(Daq)); %If LL=0 Boundary Concentration is Tank Concentration
 
-         %Flux Matching       
-         Sb(end)=S-LL*De*(bflux/(Daq)); %If LL=0 Boundary Concentration is Tank Concentration
-if Sb(end)<0
-    Sb(end)=Sbold(end);
-end
-         %Non Zero Condition
+           %Flux Calculations
+           bflux=(Sb(end)-Sb(end-1))/dz; %Biofilm Flux at boundary
+           Sbold=Sb;
+           
+        %Non Zero Condition
         Sb(Sb < 0) = 0;
         
-%          if max(abs(Sb-Sbold))<tol
-%              break
-%          end
-         Sbold=Sb;
-         
+          if max(abs(Sb-Sbold))<tol
+              break
+          end
 end
 Cs=Sb(end); %output Surface Concentration
 end
