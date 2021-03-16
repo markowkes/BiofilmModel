dafb0f6e3736b0857b1dff48de0ccc9b6cddd833
takumikammerzell <takumi.kamm@live.com>
2021-03-16 16:53:59 -0600

    Added 'mu' theory, update mu function to include different growth rate equations

--- mu.m
+++ mu.m
@@ -1,9 +1,12 @@
 function [mu] = mu(S,param)
 %% This function will allow for mu to be calculated 
-% This function calls on the Monod Growth Kinetics Equation to calculate an
+% This function calls various growth models to calculate an
 % array of growth rates mu for any array 'S'
 mumax=param.mumax;
 Km=param.Km;
 
-mu = ((mumax*S)./(Km+S));
+mu = ((mumax*S)./(Km+S));                      % Monod Growth Rate Equation
+% mu2 = ((mumax*Sa)./(Kma+Sa))*(Sb./(Kmb+Sb));    % Double Monod Growth Rate Equation
+% mu3 = ((mumax*Sa)./(Kma+Sa))*(1./(1+(Sb/Kmb))); % Inhibition Growth Rate Equation
+% mu4 = 0;                                        % None                 
 end
