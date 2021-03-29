56689f54b7cddf3ec12b5c19cd9c753b88157e82
austeneriksson <78045228+austeneriksson@users.noreply.github.com>
2021-03-28 21:42:44 -0600

    Different numerical integration technique to compute Vg as trapezoidal integration will always compute too large of a value and therefore lead to larger than realistic biomass concentrations within the tank as Vg affects the new Lf, and the new Lf affects how much detachment of biomass there is at the next time-step.

--- lf.m
+++ lf.m
@@ -9,16 +9,26 @@ function [Lf,Vdet]=lf(Sb,Lf_old,dt,dz,param)
 
 Kdet=param.Kdet;
 
-%Growth
+%% Growth
+
+% %Trapzoidal Integration Method
+% Vg=0; %initial growth velocity
+% for i=1:length(Sb)-1
+%     Vg=Vg+dz*((mu(Sb(i),param)+mu(Sb(i+1),param))/2);
+% end
+
+%Midpoint Integration Method
 Vg=0; %initial growth velocity
-for i=1:length(Sb)-1
-    Vg=Vg+dz*((mu(Sb(i),param)+mu(Sb(i+1),param))/2); %trapezoidal integration method for new growth velocity of biofilm
+i=1;
+while i<=length(Sb)-2
+    Vg=Vg+dz*(mu(Sb(i),param)+mu(Sb(i+2),param));
+    i=i+2;
 end
 
-%Detachment
+%% Detachment
 Vdet=Kdet*Lf_old^2; %New %Velocity of mass leaving biofilm into bulk liquid
 
-%Biofilm Thickness
+%% Biofilm Thickness
 Lf=Lf_old+dt*(Vg-Vdet); %New Biofilm thickness at instant
 
 end
