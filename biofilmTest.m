cc607078a3b5db9320debf07c1638a5ddfa4d759
Mark Owkes <mark.owkes@montana.edu>
2021-03-30 11:08:22 -0600

    Fixed test cases to work with code changes.

--- biofilmTest.m
+++ biofilmTest.m
@@ -4,23 +4,6 @@ clear; clc
 tests = functiontests(localfunctions);
 end
 
-%% Test flux matching
-function test_diffusion_flux(testCase)
-% Run test
-param=cases(1);
-Nz=50;
-Sbold=linspace(0,5,Nz);
-S=10;
-dz=1e-7;
-[~,Sb,bflux,flux]=biofilmdiffusion_fd(Sbold,S,Nz,dz,param);
-% Analyze result
-figure(1); clf(1)
-plot(Sb)
-actSolution = bflux-flux;
-expSolution = 0;
-tol=1e-12;
-verifyLessThan(testCase,abs(actSolution-expSolution),tol)
-end
 
 %% Test when LL=0 
 function test_diffusion_zeroLL(testCase)
@@ -31,7 +14,8 @@ Nz=50;
 Sbold=linspace(0,5,Nz);
 S=10;
 dz=1e-7;
-[~,Sb,~,~]=biofilmdiffusion_fd(Sbold,S,Nz,dz,param);
+t=0;
+[Sb,~]=biofilmdiffusion_fd(Sbold,S,Nz,dz,t,param);
 % Analyze result
 figure(1); clf(1)
 plot(Sb)
@@ -49,7 +33,8 @@ Nz=50;
 Sbold=linspace(0,5,Nz);
 S=10;
 dz=1e-7;
-[~,Sb,~,~]=biofilmdiffusion_fd(Sbold,S,Nz,dz,param);
+t=0;
+[Sb,~]=biofilmdiffusion_fd(Sbold,S,Nz,dz,t,param);
 % Analyze result
 figure(1); clf(1)
 plot(Sb)
