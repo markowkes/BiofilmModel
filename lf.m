bd3e3a0739558ef5b49a989c7f94da112ef61f5b
austeneriksson <78045228+austeneriksson@users.noreply.github.com>
2021-03-31 13:15:48 -0600

    Added Simpson's integration and check for odd grid points

--- lf.m
+++ lf.m
@@ -17,13 +17,23 @@ Kdet=param.Kdet;
 %     Vg=Vg+dz*((mu(Sb(i),param)+mu(Sb(i+1),param))/2);
 % end
 
-%Midpoint Integration Method
-Vg=0; %initial growth velocity
-i=1;
-while i<=length(Sb)-2
-    Vg=Vg+dz*(mu(Sb(i),param)+mu(Sb(i+2),param));
-    i=i+2;
+% %Midpoint Integration Method
+% Vg=0; %initial growth velocity
+% i=1;
+% while i<=length(Sb)-2
+%     Vg=Vg+dz*(mu(Sb(i),param)+mu(Sb(i+2),param));
+%     i=i+2;
+% end
+
+%Simpsons Integration
+Vg=0;
+if rem(length(Sb),2)~=1
+    fprintf("grid size needs to be odd!\n")
 end
+Vg=(dz/3)*(mu(Sb(1),param) + mu(Sb(end),param) ...
+   + 4*sum(mu(Sb(2:2:end-1),param)) ...
+   + 2*sum(mu(Sb(3:2:end-2),param)));
+
 
 %% Detachment
 Vdet=Kdet*Lf_old^2; %New %Velocity of mass leaving biofilm into bulk liquid
